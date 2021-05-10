8c8f537f80326a9af75813709fb222e5ca176cb9
gizem <gizemsacal@gmail.com>
2021-05-10 16:23:23 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.TreeMap;
+
+class Main {
+    public static void main(String[] args) {
+
+        TreeMap<String, Integer> numbers = new TreeMap<>();
+        numbers.put("First", 1);
+        numbers.put("Second", 2);
+        numbers.put("Third", 3);
+        numbers.put("Fourth", 4);
+        System.out.println("TreeMap: " + numbers);
+
+        System.out.println("\nUsing tailMap() Method:");
+        // Using tailMap() with default booleanValue
+        System.out.println("Without boolean value: " + numbers.tailMap("Second"));
+
+        // Using tailMap() with specified booleanValue
+        System.out.println("With boolean value: " + numbers.tailMap("Second", false));
+
+    }
+}
